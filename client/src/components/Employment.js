6b8dffcfc08f1e4b5e83fd22579e6b2836cbf9a9
kc15781 <kc15781@my.bristol.ac.uk>
2020-04-19 18:29:52 +0800

    fixed svg size

--- client/src/components/Employment.js
+++ client/src/components/Employment.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileSignature , faDollarSign, faBook} from '@fortawesome/free-solid-svg-icons'
 import { Row, Col, Container } from 'react-bootstrap';
-import './Stylesheet/Stylesheet_employment.css'
+
 export default class Employment extends Component {
     constructor(props) {
         super(props);
@@ -15,11 +15,11 @@ export default class Employment extends Component {
     render() {
         return (
             <>
-                <Container className="mt-5">
+                <Container className="mt-5 text-center">
                     <Row>
-                        <Col><FontAwesomeIcon className=" " icon={faFileSignature} /></Col>
-                        <Col><FontAwesomeIcon className=" " icon={faDollarSign} /></Col>
-                        <Col><FontAwesomeIcon className=" " icon={faBook} /></Col>
+                        <Col><FontAwesomeIcon  style={{width:"250px", height:"250px"}}  icon={faFileSignature} /></Col>
+                        <Col><FontAwesomeIcon  style={{width:"250px", height:"250px"}} icon={faDollarSign} /></Col>
+                        <Col><FontAwesomeIcon  style={{width:"250px", height:"250px"}} icon={faBook} /></Col>
                     </Row>
                 </Container>
             </>
